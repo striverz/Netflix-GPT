dbc787975fdf92dc91ecbc3c50080978ed17e545
striverz <striverz100x@gmail.com>
2024-10-23 15:29:02 +0530

    added the validation for the signin/signup form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState ,useRef} from 'react'
 import "./Login.css"
+import {formValidation} from "../../utils/formValidation"
 
 const Login = () => {
 
@@ -13,18 +14,23 @@ const Login = () => {
     const password=useRef(null);
 
     const handleSubmit=()=>{
-        console.log(email.current.value);
-        console.log(password);
-    }
+
+        const validationData=formValidation(email.current.value,password.current.value);
+        console.log(validationData);
+        setErrorMessage(validationData);
+    } 
+
+    const [errorMessage,setErrorMessage]=useState(null);
 
   return (
     <div className="login">
         <form className="login-form" onSubmit={(e)=>e.preventDefault()}>
-            <h2>{isSignIn ? "Sign In" : "Sign Out"}</h2>
+            <h2>{isSignIn ? "Sign In" : "Sign Up"}</h2>
             {!isSignIn &&<input type="text" placeholder="Full Name"></input>}
             <input type="text"  ref={email}placeholder="Email Address"></input>
             <input type="text"  ref={password}placeholder='Password'></input>
-            <button onClick={handleSubmit}>{isSignIn ? "Sign In" : "Sign Out"}</button>
+            {errorMessage!=null ? <p className="is-valid">{errorMessage}</p> :null}
+            <button onClick={handleSubmit}>{isSignIn ? "Sign In" : "Sign Up"}</button>
             <h5 onClick={handleSignIn}>{isSignIn ? "New to Netflix? Sign up now." : "Already have Account? Sign in now."}</h5>
 
         </form>
